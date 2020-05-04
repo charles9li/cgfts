8389a586c509071dbeaff15e778952a202fac35f
Charles Li <charles9000li@gmail.com>
2020-05-03 17:51:13 -0700

    having problems with length units

--- cg_water/openmm_water.py
+++ cg_water/openmm_water.py
@@ -3,14 +3,21 @@ from simtk.openmm import *
 from simtk.unit import *
 
 
+# Parameters
+temperature = 300.0*kelvin
+
 # Create topology
 pdb = PDBFile("tip3p.pdb")
 forcefield = ForceField("tip3p.xml")
 system = forcefield.createSystem(pdb.topology, nonbondedMethod=PME,
                                  nonbondedCutoff=1*nanometer, constraints=HBonds)
-integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+integrator = LangevinIntegrator(temperature, 1/picosecond, 0.002*picoseconds)
 simulation = Simulation(pdb.topology, system, integrator)
 simulation.context.setPositions(pdb.positions)
+simulation.context.setVelocitiesToTemperature(temperature)
 simulation.minimizeEnergy()
 simulation.reporters.append(DCDReporter(file="water.dcd", reportInterval=10))
-simulation.step(1000)
+simulation.reporters.append(StateDataReporter(file="water.csv", reportInterval=10,
+                                              step=True, time=True,
+                                              temperature=True, volume=True, density=True))
+simulation.step(10000)
--- cg_water/srel_water.py
+++ cg_water/srel_water.py
@@ -1,10 +1,11 @@
 from collections import OrderedDict as ODict
 import mdtraj as md
 import numpy as np
+import os
 import sim
 
 # Parameters
-Units = sim.units.MKSUnits
+Units = sim.units.DimensionlessUnits
 AAtrajs = ["water.dcd"]
 AAtops = ["tip3p.pdb"]
 
@@ -14,6 +15,33 @@ steepestIter = 0
 sysName = "water"
 NMolsDicts = [[('H2O', 895)]]
 MolTypesDicts = [{'H2O': ['H2O']}]
+
+Temp = 8.314e-3*300.
+dt = 0.01
+IntParams = {'TimeStep': dt, 'LangevinGamma': 1/(100*dt)}
+
+StepsEquil = 0.5/dt
+StepsProd = 0.5/dt
+StepsStride = 1
+
+# Default Simulation Package Settings
+sim.export.lammps.NeighOne = 8000
+sim.export.lammps.UseTable2 = True
+sim.export.lammps.InnerCutoff = 1.e-6
+sim.export.lammps.NPairPotentialBins = 1000
+sim.export.lammps.LammpsExec = 'lmp_omp'
+sim.export.lammps.UseLangevin = True
+sim.export.lammps.OMP_NumThread = 8
+sim.export.lammps.TableInterpolationStyle = 'spline'    # More robust than spline for highly CG-ed systems
+sim.srel.optimizetrajlammps.LammpsDelTempFiles = False
+sim.srel.optimizetrajlammps.UseLangevin = True
+sim.export.omm.platformName = 'CUDA'                    # or 'OpenCL' or 'GPU' or 'CUDA'
+sim.export.omm.device = -1                              # -1 is default, let openmm choose its own platform.
+sim.export.omm.NPairPotentialKnots = 500                # number of points used to spline-interpolate the potential
+sim.export.omm.InnerCutoff = 0.001                      # 0.001 is default. Note that a small value is not necessary, like in the lammps export, because the omm export interpolates to zero
+sim.srel.optimizetrajomm.OpenMMStepsMin = 0 #number of steps to minimize structure, 0 is default
+sim.srel.optimizetrajomm.OpenMMDelTempFiles = False #False is Default
+sim.export.omm.UseTabulated = True
 
 # ============== #
 # Map trajectory #
@@ -39,9 +67,10 @@ for i, CGatomType in enumerate(CGatomTypes):
     Atom2 = i
     this_map = sim.atommap.AtomMap(Atoms1, Atom2)
     Map.append(this_map)
-traj.save("water.lammpstrj")
-Trj = sim.traj.Lammps("water.lammpstrj")
-MappedTrj = sim.traj.Mapped(Trj, Map, AtomNames=CGatomTypes, BoxL=traj.unitcell_lengths[0][0]*10.)
+# traj.save_lammpstrj("water.lammpstrj")
+Trj = sim.traj.Lammps("water.lammpstrj", Units=Units)
+BoxL = traj.unitcell_lengths[0][0]
+MappedTrj = sim.traj.Mapped(Trj, Map, AtomNames=CGatomTypes, BoxL=BoxL)
 
 # ============= #
 # Create system #
@@ -66,8 +95,62 @@ for MolName, NMol in NMolsDict.items():
     MolType = sim.chem.MolType(MolName, AtomsInMol)
     MolTypes.append(MolType)
 world = sim.chem.World(MolTypes, Dim=3, Units=Units)
-sys = sim.system.System(world, Name=sysName)
+Sys = sim.system.System(world, Name=sysName)
+Sys.BoxL = BoxL
 
-# optimizer = sim.srel.OptimizeMultiTrajClass(opts, Weights=weights)
-# optimizer.FilePrefix = ("{}".format(sysName))
-# optimizer.RunConjugateGradient(MaxIter=maxIter, SteepestIter=steepestIter)
+# Add molecules to system
+for i, MolType in enumerate(MolTypes):
+    NMol = NMolsDict[MolType.Name]
+    for j in range(NMol):
+        Sys += MolType.New()
+Sys.ForceField.Globals.Charge.Fixed = True
+
+# Add forcefield
+Forcefield = []
+P = sim.potential.LJ(Sys, Filter=sim.atomselect.Pairs, Label="LJ", Cut=1.4, Sigma=0.3, Epsilon=1.0)
+Forcefield.append(P)
+Sys.ForceField.extend(Forcefield)
+
+# Set up the histograms
+for P in Sys.ForceField:
+    P.Arg.SetupHist(NBin=10000, ReportNBin=100)
+# lock and load
+Sys.Load()
+
+# Initial positions and velocities
+sim.system.positions.CubicLattice(Sys)
+sim.system.velocities.Canonical(Sys, Temp=Temp)
+
+# Configure integrator
+Int = Sys.Int
+Int.Method = Int.Methods.VVIntegrate
+Int.Method.TimeStep = IntParams['TimeStep']
+Int.Method.Thermostat = Int.Method.ThermostatLangevin
+Int.Method.LangevinGamma = IntParams['LangevinGamma']
+
+# ================ #
+# Create optimizer #
+# ================ #
+
+print(Sys.NMol)
+print(Sys.NAtom)
+print(Sys.NDOF)
+print(Sys.Atom)
+
+Map = sim.atommap.PosMap()
+for i, a in enumerate(Sys.Atom):
+    Map.append(sim.atommap.AtomMap(Atoms1=i, Atom2=a))
+Opt = sim.srel.optimizetrajomm.OptimizeTrajOpenMMClass(Sys, Map, Beta=1./(Units.kB*Sys.TempSet),
+                                                       Traj=MappedTrj, FilePrefix="{}".format(Sys.Name),
+                                                       TempFileDir=os.getcwd(),
+                                                       UseTarHists=False)
+Opt.ConstrainNeutralCharge()
+Opt.StepsEquil = StepsEquil
+Opt.StepsProd = StepsProd
+Opt.StepsStride = StepsStride
+
+Opts = [Opt]
+Weights = [1.]*len(Opts)
+Optimizer = sim.srel.OptimizeMultiTrajClass(Opts, Weights=Weights)
+Optimizer.FilePrefix = ("{}".format(sysName))
+Optimizer.RunConjugateGradient(MaxIter=maxIter, SteepestIter=steepestIter)
