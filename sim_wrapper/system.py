9e5d1ce69ff7098dc0900291e4c6ccbd40d16a24
Charles Li <charles9000li@gmail.com>
2021-01-12 13:44:29 -0800

    added system for cg

--- sim_wrapper/system.py
+++ sim_wrapper/system.py
@@ -1,11 +1,15 @@
 from collections import OrderedDict
+import os
 
 from scipy.constants import N_A, R
+import mdtraj as md
 import numpy as np
 
 import sim
 
-__all__ = ['SystemRun']
+from sim_wrapper.utils import *
+
+__all__ = ['SystemCG', 'SystemRun']
 
 
 # default Simulation Package Settings
@@ -74,72 +78,256 @@ class BaseSystem(object):
     def load_params_from_file(self, filename):
         self._system.ForceField.SetParamString(open(filename, 'r').read())
 
+    def run(self, *args, **kwargs):
+        pass
+
 
 class SystemCG(BaseSystem):
 
-    def __init__(self, AA_traj, AA_top, temperature, pressure, cut=2.5, stride=1):
+    def __init__(self, temperature, pressure, cut=2.5):
         super(SystemCG, self).__init__(temperature, pressure, cut=cut)
-        raise NotImplementedError("the {} class has not been implemented".format(type(self).__name__))
+        self._traj_list = []
+        self._residue_map = {}
+        self._systems = []
+        self._optimizers = []
 
+    def add_trajectory(self, dcd_list, top, stride=1):
+        if isinstance(dcd_list, str):
+            dcd_list = [dcd_list]
+        traj_list = [md.load_dcd(dcd, top=top, stride=stride) for dcd in dcd_list]
+        traj_union = md.join(traj_list)
+        traj_union.xyz = traj_union.xyz / 10.
+        traj_union.unitcell_lengths = traj_union.unitcell_lengths / 10.
+        self._traj_list.append(traj_union)
 
-_OPERATOR_PRECEDENCE = {'+': 2,
-                        '*': 3}
-_OPERATORS = list(_OPERATOR_PRECEDENCE.keys())
-_DELIMITERS = ['(', ')']
+    def add_residue_map(self, residue_name, bead_name_list, num_atoms_per_bead, atom_masses):
+        self._residue_map[residue_name] = (bead_name_list, num_atoms_per_bead, atom_masses)
 
+    def create_system(self):
+        CG_atom_type_dict = {}
+        system_index = 0
+        for traj in self._traj_list:
 
-def tokenize_expr(expr):
-    token_list = expr.split()
-    for char in _OPERATORS + _DELIMITERS:
-        temp_list = []
-        for token in token_list:
-            token = [t.strip() for t in token.split(char)]
-            token_split = [char] * (len(token) * 2 - 1)
-            token_split[0::2] = token
-            temp_list += token_split
-        token_list = temp_list
-    return [t for t in token_list if t]
+            # load topology
+            topology = traj.topology
 
+            # create map
+            Map = sim.atommap.PosMap()
 
-def create_stack(token_list):
-    output = []
-    stack = []
-    for token in token_list:
-        if token in _OPERATORS:
-            while len(stack) > 0 and stack[-1] != '(' and _OPERATOR_PRECEDENCE[stack[-1]] >= _OPERATOR_PRECEDENCE[token]:
-                output.append(stack.pop())
-            stack.append(token)
-        elif token == '(':
-            stack.append(token)
-        elif token == ')':
-            while stack[-1] != '(':
-                output.append(stack.pop())
-            stack.pop()
-        else:
-            output.append(token)
-    while len(stack) > 0:
-        output.append(stack.pop())
-    return output
+            # map CG bead indices to
+            CG_bead_index = 0
+            AA_to_CG = {}
+            CG_to_AA = {}
+            CG_to_chain = {}
+            chain_to_CG = {}
+            CGAtomNameList = []
+            MolTypeList = []
+            MolTypeDict = OrderedDict()
+            MolNumDict = OrderedDict()
+            for chain in topology.chains:
 
+                bead_type_list = []
+                bead_index_list = []
 
-def evaluate_output(output):
-    stack = []
-    for item in output:
-        if item in _OPERATORS:
-            op2 = stack.pop()
-            op1 = stack.pop()
-            if item == '+':
-                stack.append(op1 + op2)
-            elif item == '*':
-                stack.append(op1 * op2)
-        else:
-            try:
-                stack.append(int(item))
-            except ValueError:
-                stack.append([item])
-    if len(stack) != 1:
-        raise ValueError("Malformed expression.")
-    return stack.pop()
+                for residue in chain.residues:
+
+                    # get corresponding data from residue_map
+                    try:
+                        bead_types, num_atoms_per_bead, masses_residue = self._residue_map[residue.name]
+                    except KeyError:
+                        raise ValueError("Cannot find {} residue in residue_map provided.".format(residue.name))
+
+                    # check that bead_types and num_atoms_per_bead are the same length
+                    if len(bead_types) != len(num_atoms_per_bead):
+                        raise ValueError("Bead type list and atoms per bead list "
+                                         "don't have same length for {} residue".format(residue.name))
+
+                    # get indices of heavy atoms in residue
+                    heavy_atom_indices = topology.select('resid {} and element != H'.format(residue.index))
+
+                    # check that sum of num_atoms_per_bead equals the number of heavy atoms
+                    if np.sum(num_atoms_per_bead) != len(heavy_atom_indices):
+                        raise ValueError("Sum of atoms per bead list does not match the "
+                                         "number of heavy atoms in residue {}.".format(residue.name))
+
+                    # check that sum of num_atoms_per_bead equals length of mass of heavy atoms
+                    if np.sum(num_atoms_per_bead) != len(masses_residue):
+                        raise ValueError("Sum of atoms per bead list does not match the "
+                                         "number of heavy atom masses in residue {}.".format(residue.name))
+
+                    # create maps
+                    indices_split = np.split(heavy_atom_indices, np.cumsum(num_atoms_per_bead[:-1]))
+                    masses_split = np.split(masses_residue, np.cumsum(num_atoms_per_bead[:-1]))
+                    for bead_type, indices, masses in zip(bead_types, indices_split, masses_split):
+                        AtomMap = sim.atommap.AtomMap(list(indices), CG_bead_index, Mass1=masses, Atom2Name=bead_type)
+                        Map.append(AtomMap)
+                        if bead_type not in CG_atom_type_dict.keys():
+                            CG_atom_type_dict[bead_type] = sim.chem.AtomType(bead_type)
+                        for i in indices:
+                            AA_to_CG[i] = CG_bead_index
+                        CG_to_AA[CG_bead_index] = indices
+                        CG_to_chain[CG_bead_index] = chain.index
+                        bead_index_list.append(CG_bead_index)
+                        bead_type_list.append(CG_atom_type_dict[bead_type])
+                        CGAtomNameList.append(bead_type)
+                        CG_bead_index += 1
+
+                chain_to_CG[chain.index] = bead_index_list
+
+                MolType = sim.chem.MolType(str(chain.index), bead_type_list)
+                if tuple(bead_type_list) not in MolTypeDict.keys():
+                    MolTypeDict[tuple(bead_type_list)] = MolType
+                    MolNumDict[tuple(bead_type_list)] = 1
+                else:
+                    MolNumDict[tuple(bead_type_list)] += 1
+                MolTypeList.append(MolTypeDict[tuple(bead_type_list)])
+
+            # create mapped trajectory
+            print("start mapping trajectory")
+            filename = "{}.lammpstrj".format("system")
+            traj.save_lammpstrj(filename)
+            Trj = sim.traj.Lammps(filename)
+            BoxL = traj.unitcell_lengths[0][0]
+            MappedTrj = sim.traj.Mapped(Trj, Map, AtomNames=CGAtomNameList, BoxL=BoxL)
+            print("finish mapping trajectory")
+
+            # add bonds to MolTypes
+            print("start adding bonds")
+            BondTypes = []
+            for bond in topology.bonds:
+                AA_index_1 = bond[0].index
+                AA_index_2 = bond[1].index
+                CG_index_1 = AA_to_CG[AA_index_1]
+                CG_index_2 = AA_to_CG[AA_index_2]
+                if CG_index_1 != CG_index_2:
+                    chain_index = CG_to_chain[CG_index_1]
+                    MolType = MolTypeList[chain_index]
+                    bead_index_list = chain_to_CG[chain_index]
+                    try:
+                        MolType.Bond(bead_index_list.index(CG_index_1), bead_index_list.index(CG_index_2))
+                    except ValueError:
+                        pass
+                    AtomType1 = CGAtomNameList[CG_index_1]
+                    AtomType2 = CGAtomNameList[CG_index_2]
+                    BondPair = tuple(np.sort([AtomType1, AtomType2]))
+                    if BondPair not in BondTypes:
+                        BondTypes.append(BondPair)
+            print(BondTypes)
+            print("finish adding bonds")
+
+            # define world
+            print("creating world")
+            World = sim.chem.World(MolTypeDict.values(), Dim=3, Units=sim.units.DimensionlessUnits)
+
+            # create system
+            print("creating system")
+            Sys = sim.system.System(World, Name="system")
+
+            # add molecules to system
+            print("adding molecules to system")
+            for i, MolType in zip(MolNumDict.values(), MolTypeDict.values()):
+                for _ in range(i):
+                    Sys += MolType.New()
+
+            # Set system box length
+            print("seting system box length")
+            Sys.BoxL = BoxL
+
+            # Set system temperature and charges
+            Sys.TempSet = self._temperature
+            Sys.PresSet = self._pressure
+            Sys.ForceField.Globals.Charge.Fixed = True
+
+            # initialize force field
+            ForceField = []
+
+            # create Gaussian potentials
+            print("creating Gaussian potentials")
+            BeadNameList = CG_atom_type_dict.keys()
+            for i, BeadName1 in enumerate(BeadNameList):
+                for BeadName2 in BeadNameList[i:]:
+                    BeadName1_sorted, BeadName2_sorted = np.sort([BeadName1, BeadName2])
+                    Label = "Gaussian_{}{}".format(BeadName1_sorted, BeadName2_sorted)
+                    BeadType1 = CG_atom_type_dict[BeadName1_sorted]
+                    BeadType2 = CG_atom_type_dict[BeadName2_sorted]
+                    Filter = sim.atomselect.PolyFilter([BeadType1, BeadType2])
+                    Gaussian = sim.potential.LJGaussian(Sys, Cut=self._cut, Filter=Filter,
+                                                        B=1.0, Kappa=1.0, Dist0=0.0, Sigma=1.0, Epsilon=0.0,
+                                                        Label=Label)
+                    # TODO: implement way to fix B
+                    Gaussian.Param.Kappa.Fixed = True
+                    Gaussian.Param.Dist0.Fixed = True
+                    Gaussian.Param.Sigma.Fixed = True
+                    Gaussian.Param.Epsilon.Fixed = True
+                    ForceField.append(Gaussian)
+
+            # create Bonded potentials
+            print("creating bonded potentials")
+            for BondType in BondTypes:
+                BeadName1, BeadName2 = BondType
+                Label = "Bonded_{}{}".format(BeadName1, BeadName2)
+                BeadType1 = CG_atom_type_dict[BeadName1]
+                BeadType2 = CG_atom_type_dict[BeadName2]
+                Filter = sim.atomselect.PolyFilter([BeadType1, BeadType2], Bonded=True)
+                Bonded = sim.potential.Bond(Sys, Filter=Filter,
+                                            Dist0=1.0, FConst=1.0,
+                                            Label=Label)
+                # TODO: implement way to fix dist0 and k
+                ForceField.append(Bonded)
+
+            # add potentials to forcefield
+            Sys.ForceField.extend(ForceField)
+
+            # set up the histograms
+            for P in Sys.ForceField:
+                P.Arg.SetupHist(NBin=10000, ReportNBin=100)
+
+            Sys.World.GetBondOrdMatrix(ShowRigid=True)
+
+            # lock and load
+            print("loading system")
+            Sys.Load()
+
+            # initial positions and velocities
+            print("initializing positions and velocities")
+            sim.system.positions.CubicLattice(Sys)
+            sim.system.velocities.Canonical(Sys, Temp=self._temperature)
+
+            # configure integrator
+            print("configuring integrator")
+            Int = Sys.Int
+            Int.Method = Int.Methods.VVIntegrate
+            Int.Method.TimeStep = 0.005
+            Int.Method.Thermostat = Int.Method.ThermostatLangevin
+            Int.Method.LangevinGamma = 1.0
+            Int.Method.Barostat = Int.Method.BarostatMonteCarlo
+
+            # create optimizer
+            Map = sim.atommap.PosMap()
+            for i, a in enumerate(Sys.Atom):
+                Map.append(sim.atommap.AtomMap(Atoms1=i, Atom2=a))
+            Opt = sim.srel.optimizetrajomm.OptimizeTrajOpenMMClass(Sys, Map,
+                                                                   Traj=MappedTrj, FilePrefix="{}{}".format(Sys.Name, system_index),
+                                                                   TempFileDir=os.getcwd(),
+                                                                   UseTarHists=False)
+            Opt.ConstrainNeutralCharge()
+            self._optimizers.append(Opt)
+            self._systems.append(Sys)
+            system_index += 1
+
+    def load_params_from_file(self, filename):
+        for system in self._systems:
+            system.ForceField.SetParamString(open(filename, 'r').read())
+
+    def run(self, steps_equil, steps_prod, steps_stride):
+        for opt in self._optimizers:
+            opt.StepsEquil = steps_equil
+            opt.StepsProd = steps_prod
+            opt.StepsStride = steps_stride
+        weights = [1.]*len(self._optimizers)
+        optimizer = sim.srel.OptimizeMultiTrajClass(self._optimizers, Weights=weights)
+        optimizer.FilePrefix = ("system")
+        optimizer.RunConjugateGradient(MaxIter=None, SteepestIter=0)
 
 
 class SystemRun(BaseSystem):
@@ -160,7 +348,7 @@ class SystemRun(BaseSystem):
                              'mA12': ['Bplma', 'C6', 'E6']}
 
     def add_polyacrylate(self, sequence, num_mol=1):
-        monomer_list = evaluate_output(create_stack(tokenize_expr(sequence)))
+        monomer_list = acrylate_sequence_to_list(sequence)
         bead_name_list = []
         for monomer in monomer_list:
             bead_name_list.extend(self._MONOMER_TO_BEAD_NAME[monomer])
