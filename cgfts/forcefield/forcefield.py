98494bca54bc98986e3e253db4e023861e3647ad
Charles Li <charles9000li@gmail.com>
2021-02-13 11:19:46 -0800

    added pressure for 313.15 K

--- cgfts/forcefield/forcefield.py
+++ cgfts/forcefield/forcefield.py
@@ -47,6 +47,7 @@ _SMEAR_LENGTHS = {'A4':     {313.15: 0.6070253571898435,
 # pressure corresponding to each temperature
 _DEFAULT_PRESSURES = {293.15: 4500.0,
                       313.15: 3866.4,
+                      353.15: 3157.8,
                       373.15: 2738.8}
 
 
