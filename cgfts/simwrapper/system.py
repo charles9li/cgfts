2db85390affdb6b8f20a1b588d0a08ee4bd538b5
Charles Li <charles9000li@gmail.com>
2021-09-08 07:51:29 -0700

    fixed bug

--- cgfts/simwrapper/system.py
+++ cgfts/simwrapper/system.py
@@ -734,12 +734,11 @@ class SystemRun(BaseSystem):
             force_field.append(bonded)
 
         # add external potential
-        if self._ext_pot is not None:
-            Filter = sim.atomselect.PolyFilter([self._bead_type_dict[self._ext_pot[0]]])
-            Sinusoidal = sim.potential.ExternalSinusoid(self._system, Label="sin_{}".format(self._ext_pot[0]),
-                                                        Filter=Filter,
-                                                        UConst=self._ext_pot[1], NPeriods=1.0,
-                                                        PlaneAxis=self._ext_pot[2])
+        for ext_pot in self._external_potentials:
+            Filter = sim.atomselect.PolyFilter([self._bead_type_dict[ext_pot[0]]])
+            Sinusoidal = sim.potential.ExternalSinusoid(self._system, Label="sin_{}".format(ext_pot[0]), Filter=Filter,
+                                                        UConst=ext_pot[1], NPeriods=1.0,
+                                                        PlaneAxis=ext_pot[2])
             force_field.append(Sinusoidal)
 
         # add potentials to force field
