af7891bfc12ba402dccbc9eb78f6011d1312fcca
Charles Li <charles9000li@gmail.com>
2021-10-06 10:56:44 -0700

    initial commit

--- cgfts/utils/__init__.py
+++ cgfts/utils/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 from .blockify import blockify
 from .sorted_binary_set import SortedBinarySet
 from .sequence_parser import *
+from .split_path import split_path
