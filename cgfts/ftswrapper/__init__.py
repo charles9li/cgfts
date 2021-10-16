fee7f1cce5e573e7491e8dc5145216c10a8b79f9
Charles Li <charles9000li@gmail.com>
2021-10-15 23:32:32 -0700

    added function to find compositions that match chemical potentials

--- cgfts/ftswrapper/__init__.py
+++ cgfts/ftswrapper/__init__.py
@@ -2,5 +2,6 @@ from __future__ import absolute_import
 
 from .find_chain_density import find_chain_density
 from .fts import FTS
+from .match_chemical_potentials import match_chemical_potentials
 from .operator_data import OperatorData
 from .run_fts import RunFTS
